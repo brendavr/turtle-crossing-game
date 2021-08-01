d80194e679ff2c3ebb8dd3214995ebec2943ad40
brendavr <73349902+brendavr@users.noreply.github.com>
2021-08-01 10:05:34 -0700

    Add files via upload

---
+++ car_manager.py
@@ -0,0 +1,35 @@
+from turtle import Turtle
+import random
+
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple", "dimgray", "LightSalmon", "DarkOrchid", "DarkBlue",
+          "DarkCyan", "chocolate", "aquamarine", "BlueViolet", "chartreuse"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 50
+
+
+class CarManager:
+    def __init__(self):
+        self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
+
+    def create_car(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            rand_y = random.randint(-250, 250)
+            new_car.goto(300, rand_y)
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(STARTING_MOVE_DISTANCE)
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
+
+
+
